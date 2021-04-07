edfd5e43a12fde4985f2d2d1d203cc8492c6df2c
Seth A. Johnson <sethajoh@umich.edu>
2021-04-07 14:13:59 -0400

    set up netlify and netlify cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -18,7 +18,7 @@ if (process.env.BRANCH === "preview") {
         repo: 'michigan-publishing/about-heb',
         branch: 'preview',
         auth_type: 'implicit',
-        app_id: '1ad010fc364de8a6303468218779296589f4f424dd3284e42bf954cad431395b',
+        app_id: '',
         api_root: 'https://gitlab.umich.edu/api/v4',
         base_url: 'https://gitlab.umich.edu',
         auth_endpoint: 'oauth/authorize'
@@ -33,7 +33,7 @@ if (process.env.BRANCH === "preview") {
         repo: 'michigan-publishing/about-heb',
         branch: 'master',
         auth_type: 'implicit',
-        app_id: '03d3fe1c9a36d44c83ea5d1f0382ac41333bd88052587631ae9fa15ce6b15123',
+        app_id: '858414e24ddf77a7f901ad551f195e54f2f1cedfedb8454c4f1061f17324c9c6',
         api_root: 'https://gitlab.umich.edu/api/v4',
         base_url: 'https://gitlab.umich.edu',
         auth_endpoint: 'oauth/authorize'
