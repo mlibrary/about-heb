6010beeabef52df5256d05edbc48081996932737
Jonathan McGlone <jmcglone@umich.edu>
2021-05-17 14:52:20 -0400

    Update navbar.js to match wireframe

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -36,54 +36,34 @@ const Navbar = () => {
           }
         >
           <ul className="navbar-nav float-right">
+            <li>
+              <Link to="/collection" className="nav-link text-dark">
+                The Collection
+              </Link>
+            </li>
             <li className="nav-item">
               <Link to="/about" className="nav-link text-dark">
                 About
               </Link>
             </li>
             <li>
-              <a href="https://www.fulcrum.org/heb" className="nav-link text-dark">
-                Books
-              </a>
-            </li>
-            <li>
-              <Link to="/impact" className="nav-link text-dark">
-                Impact
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="nav-link text-dark">
-                Contact
-              </Link>
-            </li>
-            <li>
               <Link to="/librarians" className="nav-link text-dark">
-                Librarians
+                For Librarians
               </Link>
             </li>
             <li>
               <Link to="/publishers" className="nav-link text-dark">
-                Publishers
+                For Publishers
               </Link>
             </li>
             <li>
               <Link to="/societies" className="nav-link text-dark">
-                Societies
+                For Societies
               </Link>
             </li>
             <li>
-              <Link to="/collection" className="nav-link text-dark">
-                Collection
-              </Link>
-            </li>
-            <li>
-              <Link to="/help" className="nav-link text-dark">
-                Help
-              </Link>
-            </li>
-            <li>
-              <Link to="/faqs" className="nav-link text-dark">
-                FAQs
+              <Link to="/impact" className="nav-link text-dark">
+                Impact
               </Link>
             </li>
           </ul>
