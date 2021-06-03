771ac43dc8ca4420e7e40cd60529e993c6ba6976
Jonathan McGlone <jmcglone@umich.edu>
2021-06-03 14:17:39 -0400

    update terms link in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -89,7 +89,7 @@ const Footer = () => {
         </div>
         <div className="row mt-3">
           <div className="col-md-6">
-          <small className="text-light d-block mb-3">© {year} ACLS Humanities Ebook · <a className="text-light" href="https://fulcrum.org/accessibility/">Accessibility</a> · <a className="text-light" href="https://fulcrum.org/preservation">Preservation</a> · <a className="text-light" href="https://fulcrum.org/privacy">Privacy</a> · <a className="text-light" href="https://fulcrum.org/terms">Terms of Service</a></small>    
+          <small className="text-light d-block mb-3">© {year} ACLS Humanities Ebook · <a className="text-light" href="https://fulcrum.org/accessibility/">Accessibility</a> · <a className="text-light" href="https://fulcrum.org/preservation">Preservation</a> · <a className="text-light" href="https://fulcrum.org/privacy">Privacy</a> · <a className="text-light" href="/terms">Terms of Service</a></small>    
           </div>
           <div className="col-md-6 text-right">
             <small className="text-white">Powered by</small> <a href="https://fulcrum.org/"><img className="fulcrum" src="/assets/fulcrum-full-white.svg" alt="Fulcrum logo" height="20" width="auto" /></a>
